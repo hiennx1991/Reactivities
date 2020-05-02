8d365f1ba9cd298b286d6762f049e78a7a970b7d
hienqualityhub <hienqualityhub@gmail.com>
2020-05-03 02:37:26 +1200

    Section 9

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,54 +1,72 @@
-import React, { FormEvent, useState } from "react";
-import { Segment, Form, Button } from "semantic-ui-react";
+import React, { FormEvent, useState, useContext, useEffect } from "react";
+import { Segment, Form, Button, Grid } from "semantic-ui-react";
 import { IActivity } from "../../../app/models/activity";
-import {v4 as uuid} from 'uuid';
+import { v4 as uuid } from "uuid";
+import ActivityStore from "../../../app/stores/activityStore";
+import { observer } from "mobx-react-lite";
+import { RouteComponentProps } from "react-router";
 
-interface IProps {
-  setEditMode: (editMode: boolean) => void;
-  activity: IActivity;
-  createActivity:(activity:IActivity)=>void;
-  editActivity:(activity:IActivity)=>void;
-  submitting:boolean
+interface DetailParams {
+  id: string;
 }
 
-
-const ActivityForm: React.FC<IProps> = ({
-  setEditMode,
-  activity: initialFormState,
-  createActivity,
-  editActivity,
-  submitting
+const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({
+  match,
+  history,
 }) => {
+  const activityStore = useContext(ActivityStore);
+  const {
+    createActivity,
+    editActivity,
+    submitting,
+    cancelFormOpen,
+    selectedActivity: initialFormState,
+    loadActivity,
+    clearActivity,
+  } = activityStore;
 
-  const initializeForm = () => {
-    if (initialFormState) {
-      return initialFormState;
-    } else {
-      return {
-        id:"",
-        title: "",
-        category: "",
-        description: "",
-        date: "",
-        city: "",
-        venue: "",
+  const [activity, setActivity] = useState<IActivity>({
+    id: "",
+    title: "",
+    category: "",
+    description: "",
+    date: "",
+    city: "",
+    venue: "",
+  });
+
+  useEffect(() => {
+    if (match.params.id && activity.id.length === 0) {
+      loadActivity(match.params.id).then(
+        () => initialFormState && setActivity(initialFormState)
+      );
+    }
+    return () => {
+      clearActivity();
+    };
+  }, [
+    loadActivity,
+    match.params.id,
+    clearActivity,
+    initialFormState,
+    activity.id.length,
+  ]);
+
+  const handleSubmit = () => {
+    if (activity.id.length === 0) {
+      let newActivity = {
+        ...activity,
+        id: uuid(),
       };
+      createActivity(newActivity).then(() =>
+        history.push(`/activities/${newActivity.id}`)
+      );
+    } else {
+      editActivity(activity).then(() =>
+        history.push(`/activities/${activity.id}`)
+      );
     }
   };
-
-  const [activity, setActivity] = useState<IActivity>(initializeForm);
-
-  const handleSubmit=()=>{
-    if(activity.id.length===0){
-      let newActivity={
-        ...activity,
-        id:uuid()
-      }
-      createActivity(newActivity);
-    }else{
-      editActivity(activity);
-    }
-  }
 
   const handleInputChange = (
     event: FormEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -57,24 +75,67 @@ const ActivityForm: React.FC<IProps> = ({
     setActivity({ ...activity, [name]: value });
   };
   return (
-    <Segment clearing>
-      <Form>
-        <Form.Input onChange={handleInputChange} name='title' placeholder="Title" value={activity.title} />
-        <Form.TextArea rows={2} onChange={handleInputChange} name='description' placeholder="Description" value={activity.description} />
-        <Form.Input onChange={handleInputChange} name='category' placeholder="Category" value={activity.category} />
-        <Form.Input type="datetime-local" onChange={handleInputChange} name='date' placeholder="Date" value={activity.date} />
-        <Form.Input onChange={handleInputChange} name='city' placeholder="City" value={activity.city} />
-        <Form.Input onChange={handleInputChange} name='venue' placeholder="Venue" value={activity.venue} />
-        <Button loading={submitting} onClick={handleSubmit} positive floated="right" type="submit" content="submit" />
-        <Button
-          onClick={() => setEditMode(false)}
-          floated="right"
-          type="button"
-          content="Cancel"
-        />
-      </Form>
-    </Segment>
+    <Grid>
+      <Grid.Column width={10}>
+        <Segment clearing>
+          <Form>
+            <Form.Input
+              onChange={handleInputChange}
+              name="title"
+              placeholder="Title"
+              value={activity.title}
+            />
+            <Form.TextArea
+              rows={2}
+              onChange={handleInputChange}
+              name="description"
+              placeholder="Description"
+              value={activity.description}
+            />
+            <Form.Input
+              onChange={handleInputChange}
+              name="category"
+              placeholder="Category"
+              value={activity.category}
+            />
+            <Form.Input
+              type="datetime-local"
+              onChange={handleInputChange}
+              name="date"
+              placeholder="Date"
+              value={activity.date}
+            />
+            <Form.Input
+              onChange={handleInputChange}
+              name="city"
+              placeholder="City"
+              value={activity.city}
+            />
+            <Form.Input
+              onChange={handleInputChange}
+              name="venue"
+              placeholder="Venue"
+              value={activity.venue}
+            />
+            <Button
+              loading={submitting}
+              onClick={handleSubmit}
+              positive
+              floated="right"
+              type="submit"
+              content="submit"
+            />
+            <Button
+              onClick={() => history.push("/activities")}
+              floated="right"
+              type="button"
+              content="Cancel"
+            />
+          </Form>
+        </Segment>
+      </Grid.Column>
+    </Grid>
   );
 };
 
-export default ActivityForm;
+export default observer(ActivityForm);
